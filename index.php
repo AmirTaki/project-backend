aa74ef71810a475eed3ec01092afe0710b11bff0
amir <amirtaki623@gmail.com>
2025-06-14 15:17:43 +0330

    add header styles at project

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $imageTable = readTable ("asus", "SELECT * FROM asus.img_menu  Where status = 10
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./src/styles/styles.css">
+    <link rel="stylesheet" href="./styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <title>Document</title>
 </head>
