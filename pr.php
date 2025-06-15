05f3446e19fc788a3bac3c2af266caaa80b481a3
amir <amirtaki623@gmail.com>
2025-06-15 14:41:28 +0330

    item-title

--- pr.php
+++ pr.php
@@ -32,52 +32,51 @@ $imageTable = readTable ("asus", "SELECT * FROM asus.img_menu  Where status = 10
                 <div class = "crossIcon"><i class="bi bi-x-lg"></i></div>
             </div>
             <?php foreach($menus as $menu)  {?>
-                <div class = "menu">
-                    <div class = "menu-title">
-                        <div class = "titleContainer">
-                            <div><?= $menu->title ?></div>
-                            <div class = "icon-title"> <i class="bi bi-chevron-down"></i></div>
-                        </div>       
-                        <div class = 'item'>
-                            <?php foreach($megaMenu as $list) { 
-                                if($list->title === $menu->title) {
-                            ?>
-                                <div class = "item-title"><?= $list->list ?>
-                                <div class = "container">
-                                        <div class = "category">
-                                            <?php 
-                                                foreach($categoryTable as $category){
-                                                    if($category->title === $menu->title && $category->list === $list->list){
-                                            ?>
-                                                <div><?= $category->category ?></div>
-                                            <?php  }}?>
-                                        </div>
-                                        <div class ="product">
-                                            <?php 
-                                                foreach($seriesTable as $series){
-                                                    if($series->title === $menu->title && $series->list === $list->list){
-                                            ?>
-                                                <div><?= $series->series ?></div>
-                                            <?php }} ?>
-                                        </div>
-                                        <div class = "img">
-                                            <?php 
-                                                foreach($imageTable as $img){
-                                                    if($img->title === $menu->title && $img->list === $list->list){
-                                            ?>
-                                                <img src="<?= $img->image ?>" alt="">
-                                            <?php }} ?>
-                                        </div>
+            <div class = "menu">
+                <div class = "menu-title">
+                    <div class = "titleContainer">
+                        <div><?= $menu->title ?></div>
+                        <div class = "icon-title"> <i class="bi bi-chevron-down"></i></div>
+                    </div>       
+                    <div class = 'item'>
+                        <?php foreach($megaMenu as $list) { 
+                            if($list->title === $menu->title) {
+                        ?>
+                            <div class = "item-title"><?= $list->list ?>
+                            <div class = "icon-item"><i class="bi bi-chevron-right"></i></div>
+                            <div class = "container">
+                                    <div class = "category">
+                                        <?php 
+                                            foreach($categoryTable as $category){
+                                                if($category->title === $menu->title && $category->list === $list->list){
+                                        ?>
+                                            <div><?= $category->category ?></div>
+                                        <?php  }}?>
+                                    </div>
+                                    <div class ="product">
+                                        <?php 
+                                            foreach($seriesTable as $series){
+                                                if($series->title === $menu->title && $series->list === $list->list){
+                                        ?>
+                                            <div><?= $series->series ?></div>
+                                        <?php }} ?>
+                                    </div>
+                                    <div class = "img">
+                                        <?php 
+                                            foreach($imageTable as $img){
+                                                if($img->title === $menu->title && $img->list === $list->list){
+                                        ?>
+                                            <img src="<?= $img->image ?>" alt="">
+                                        <?php }} ?>
                                     </div>
                                 </div>
-                            
-                            <?php }} ?>
-                        </div>
+                            </div>
+                        <?php }} ?>
                     </div>
-                </div> 
-            <?php } ?>             
+                </div>
+            </div> 
+            <?php } ?>     
         </div>
-
         <div>search icon</div>
     </div>
                                     
